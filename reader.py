85858024156bf2a1db3c208a21bde866fcbc64f7
omori <omori@uel.br>
2019-06-12 15:00:35 -0300

    Added the dataset reader and the datasets

---
+++ reader.py
@@ -0,0 +1,11 @@
+import csv
+
+def read_dataset(filename):
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        dataset = []
+        for row in reader:
+            dataset.append(row)
+
+    #retornando o dataset limpo e o volume da mochila
+    return dataset[2:], dataset[1][1]
