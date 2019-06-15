f7a873417997cf66386cae2c9c0640639212bb90
n0mori <nick33omori@gmail.com>
2019-06-15 00:16:16 -0300

    bug fix

--- population.py
+++ population.py
@@ -6,11 +6,11 @@ import random
 def gen_population(item_count, pop_size=20):
     pop = []
     for i in range(pop_size):
-        pop.append(random.sample(range(50), 50))
+        pop.append(random.sample(range(item_count), item_count))
     return pop
 
 def calculate_fitness(gene, dataset, cap):
-    fit = 0;
+    fit = 0
     for item, _ in sorted(enumerate(gene), key=lambda tpl: tpl[1]):
         weight = int(dataset[item][1])
         imp = int(dataset[item][2])
